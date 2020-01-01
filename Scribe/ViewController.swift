92bebd3734430bedb9f6ba922a1f7ee90eeae5d7
Chris Hachey <chris@amazillo.app>
2020-01-01 12:38:45 -0400

    Initial Commit

---
+++ Scribe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scribe
+//
+//  Created by Christopher Hachey on 2020-01-01.
+//  Copyright © 2020 Christopher Hachey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
