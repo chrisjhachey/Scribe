b3251293072558bc4b7543ed202334ebeeeff33e
Chris Hachey <chris@amazillo.app>
2020-03-26 12:04:55 -0300

    reconfigure for ElasticBeanstalk deploy

--- API/go/src/build.sh
+++ API/go/src/build.sh
@@ -5,6 +5,7 @@ set -xe
 go get github.com/dgrijalva/jwt-go
 go get github.com/go-sql-driver/mysql
 go get github.com/gorilla/mux
+go get github.com/chrisjhachey/Scribe/tree/UserAuth/API/go/src/api
 
 # build command
 go build -o bin/application application.go
