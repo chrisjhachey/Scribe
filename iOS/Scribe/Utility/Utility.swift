663cdbd6ce2b448a78285dbaf4fec1c46fb10e33
Chris Hachey <chris@amazillo.app>
2020-04-04 12:15:14 -0300

    Updated the UI for the HomeVC

--- iOS/Scribe/Utility/Utility.swift
+++ iOS/Scribe/Utility/Utility.swift
@@ -314,3 +314,21 @@ extension NSAttributedString.Key {
         }
     }
 }
+
+// Extension used to apply colour gradients to UIViews
+extension UIView {
+    @discardableResult
+    func applyGradient(colours: [UIColor]) -> CAGradientLayer {
+        return self.applyGradient(colours: colours, locations: nil)
+    }
+
+    @discardableResult
+    func applyGradient(colours: [UIColor], locations: [NSNumber]?) -> CAGradientLayer {
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.frame = self.bounds
+        gradient.colors = colours.map { $0.cgColor }
+        gradient.locations = locations
+        self.layer.insertSublayer(gradient, at: 0)
+        return gradient
+    }
+}
